8479db56e035b2058872dbc6842cc5f50f1d31ef
Patrick Sewell <pssewell21@outlook.com>
2019-03-22 08:00:35 -0500

    Adding problem 2

--- Problem1/Program.cs
+++ Problem1/Program.cs
@@ -6,7 +6,55 @@ namespace Problem1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DoSampleProblem();
+
+            DoProblem();
+
+            Console.Read();
+        }
+
+        private static void DoSampleProblem()
+        {
+            const int MAX_VALUE = 10;
+
+            var sum = 0;
+
+            var value = 3;
+
+            while (value < MAX_VALUE)
+            {
+                sum += value;
+
+                value += 3;
+            }
+
+            value = 5;
+
+            while (value < MAX_VALUE)
+            {
+                sum += value;
+
+                value += 5;
+            }
+
+            Console.WriteLine($"Sum of multiples of 3 and 5 below {MAX_VALUE} = {sum}");
+        }
+
+        private static void DoProblem()
+        {
+            const int MAX_VALUE = 1000;
+
+            var sum = 0;
+
+            for (int i = 0; i < MAX_VALUE; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine($"Sum of multiples of 3 and 5 below {MAX_VALUE} = {sum}");
         }
     }
 }
---
+++ Problem2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoProblem();
+
+            Console.Read();
+        }
+
+        private static void DoProblem()
+        {
+            const int MAX_VALUE = 4000000;
+
+            int sum = 0;
+
+            var term1 = 1;
+            var term2 = 2;
+            int next;
+
+            sum += term2;
+
+            while (term1 + term2 < MAX_VALUE)
+            {
+                next = term1 + term2;
+
+                if (next % 2 == 0)
+                {
+                    sum += next;
+                }
+
+                term1 = term2;
+                term2 = next;
+
+                Console.WriteLine($"Next = {next}");
+            }
+
+            Console.WriteLine($"Sum of fibonacci numbers below {MAX_VALUE} = {sum}");
+        }
+    }
+}
