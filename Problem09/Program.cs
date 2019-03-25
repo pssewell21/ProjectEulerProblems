bf15ae86da95a03cabace0abbcaa354514dedf4d
Patrick Sewell <pssewell21@outlook.com>
2019-03-24 19:34:16 -0500

    Problem 10

---
+++ Problem09/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Problem9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoSampleProblem();
+            DoProblem();
+
+            Console.Read();
+        }
+
+        #region Sample Problem
+
+        private static void DoSampleProblem()
+        {
+            var stopWatch = Stopwatch.StartNew();
+
+            const int sumOfValues = 12;
+            var breakOut = false;
+
+            for (int a = 1; a < sumOfValues; a++)
+            {
+                for (int b = a; b < sumOfValues; b++)
+                {
+                    for (int c = b; c < sumOfValues; c++)
+                    {
+                        if (a + b + c == sumOfValues)
+                        {
+                            int leftValue = (a * a) + (b * b);
+                            int rightValue = c * c;
+
+                            if (leftValue == rightValue)
+                            {
+                                Console.WriteLine($"Pythagorean triple found : {a}, {b}, {c}, Product = {a * b * c}");
+                                breakOut = true;
+                            }
+                            else
+                            {
+                                Console.WriteLine($"{a}, {b}, {c} is not a Pythagorean triple");
+                            }
+                        }
+                        else
+                        {
+                            //Console.WriteLine($"{a}, {b}, {c} do not add up to {sumOfValues}");
+                        }
+
+                        if (breakOut)
+                        {
+                            break;
+                        }
+                    }
+
+                    if (breakOut)
+                    {
+                        break;
+                    }
+                }
+
+                if (breakOut)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine($"Execution took {stopWatch.ElapsedMilliseconds / 1000D} seconds");
+        }
+
+        #endregion
+
+        private static void DoProblem()
+        {
+            var stopWatch = Stopwatch.StartNew();
+
+            const int sumOfValues = 1000;
+            var breakOut = false;
+
+            for (int a = 1; a < sumOfValues; a++)
+            {
+                for (int b = a; b < sumOfValues; b++)
+                {
+                    for (int c = b; c < sumOfValues; c++)
+                    {
+                        if (a + b + c == sumOfValues)
+                        {
+                            int leftValue = (a * a) + (b * b);
+                            int rightValue = c * c;
+
+                            if (leftValue == rightValue)
+                            {
+                                Console.WriteLine($"Pythagorean triple found : {a}, {b}, {c}, Product = {a * b * c}");
+                                breakOut = true;
+                            }
+                        }
+
+                        if (breakOut)
+                        {
+                            break;
+                        }
+                    }
+
+                    if (breakOut)
+                    {
+                        break;
+                    }
+                }
+
+                if (breakOut)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine($"Execution took {stopWatch.ElapsedMilliseconds / 1000D} seconds");
+        }
+    }
+}
---
+++ Problem10/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Problem10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoSampleProblem();
+            DoProblem();
+
+            Console.Read();
+        }
+
+        #region Sample Problem
+
+        private static void DoSampleProblem()
+        {
+            var stopWatch = Stopwatch.StartNew();
+
+            const int MAX_PRIME_VALUE = 100;
+
+            int number = 2;
+
+            long sum = 0;
+
+            while (number <= MAX_PRIME_VALUE)
+            {
+                if (IsPrime(number))
+                {
+                    sum += number;
+
+                    Console.WriteLine($"{number} is prime, new sum = {sum}");
+                }
+
+                number++;
+            }
+
+            Console.WriteLine($"Sum of prime numbers below {MAX_PRIME_VALUE} = {sum}");
+
+            Console.WriteLine($"Execution took {stopWatch.ElapsedMilliseconds / 1000D} seconds");
+        }
+
+        #endregion
+
+        private static void DoProblem()
+        {
+            var stopWatch = Stopwatch.StartNew();
+
+            const int MAX_PRIME_VALUE = 2000000;
+
+            int number = 2;
+
+            long sum = 0;
+
+            while (number <= MAX_PRIME_VALUE)
+            {
+                if (IsPrime(number))
+                {
+                    sum += number;
+
+                    //Console.WriteLine($"{number} is prime, new sum = {sum}");
+                }
+
+                number++;
+            }
+
+            Console.WriteLine($"Sum of prime numbers below {MAX_PRIME_VALUE} = {sum}");
+
+            Console.WriteLine($"Execution took {stopWatch.ElapsedMilliseconds / 1000D} seconds");
+        }
+
+        private static bool IsPrime(long number)
+        {
+
+            if (number <= 3)
+            {
+                return number > 1;
+            }
+            else if (number % 2 == 0 || number % 3 == 0)
+            {
+                return false;
+            }
+            
+            var isPrime = true;
+
+            for (long i = 5; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    isPrime = false;
+                }
+            }
+
+            //Console.WriteLine($"{number}, isPrime = {isPrime}");
+
+            return isPrime;
+        }
+    }
+}
